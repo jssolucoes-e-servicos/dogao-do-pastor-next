397c2fd95f50effc9d20281fd7d7e1b6d986b805
JS Soluções e Serviços <jssolucoeseservicos@gmail.com>
2025-07-12 00:44:07 -0300

    FIX make a loading components

---
+++ src/components/loading.tsx
@@ -0,0 +1,5 @@
+export function LoadingSpinner() {
+  return (<div className="flex items-center justify-center min-h-screen">
+    <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-orange-800"></div>
+  </div>)
+}
